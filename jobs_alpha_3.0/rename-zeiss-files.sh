76f0f8928af9ec6e6c076e64ed361006c718f573
Christopher Schmied <schmied@mpi-cbg.de>
2015-03-25 09:23:11 +0100

    Added new tools for preprocessing and enabled multiview fusion
    Extended preprocessing capabilities:
    
    * Processing og ome.tiff files:
    
    PROBLEM:OpenSPIM uses .ome.tiff format.
    
    SOLUTION: Conversion of .ome.tif into .tif is now supported.
    
    * Processing of discontinous .czi datasets:
    
    PROBLEM: When restarting the imaging in ZEN for a recording the
    resulting .czi files are discontinous and treated as different
    dataset. Thus one cannot define 1 dataset in the first processing
    step of Multiview Reconstruction.
    
    SOLUTION: To address that the .czi files are first rename into a
    conected pattern and then resaved into .tif or .zip
    
    LIMITATION: The resaving of .czi files is still based on lifeline
    version of Fiji containing an old bioformats version.
    
    * Compression:
    
    PROBLEM: limitation of storage space for transient files of
    processing
    
    SOLUTION: Resave into a compressed file using resave to ZIP
    in FIJI. This replaces the resave to .tif scripts. Allows a almost
    2x reduction in the size of the files and is promised to speed up
    the downstream processing
    
    Added fusion to multiview processing:
    
    * Added multiview fusion to pipeline:
    
    LIMITATION: The fusion is not cluster enabled and writes the output
    files as .tif. It does not write a new .xml file or add to the
    existing file. Thus there is no support via the BigDataViewer yet.

---
+++ jobs_alpha_3.0/rename-zeiss-files.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source /projects/pilot_spim/Christopher/pipeline_3.0/master_3.0
+
+source_pattern=${image_file_directory}${source_pattern}
+target_pattern=${image_file_directory}${target_pattern}
+
+# --------------------------------------------------------
+
+i=${first_index}
+t=${first_timepoint}
+t=`printf "%0${pad}d" "${t}"`
+
+while [ $i -le $last_index ]; do
+
+        for a in "${angles_renaming[@]}"; do
+                source=${source_pattern/\{index\}/${i}}
+                tmp=${target_pattern/\{timepoint\}/${t}}
+                target=${tmp/\{angle\}/${a}}
+
+                echo ${source} ${target}
+
+               	mv ${source} ${target}
+                #cp ${source} ${target}
+                let i=i+1
+
+        done
+	t=$(( 10#${t} ))
+        let t=t+1
+        t=`printf "%0${pad}d" "${t}"`
+
+done
