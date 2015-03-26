19fd9cd9bf28ab377afae33196435e9713cb0eef
Christopher Schmied <schmied@mpi-cbg.de>
2015-03-26 12:14:04 +0100

    Tested and improved renaming, resaving and master file
    Cosmetic changes in the master file for better usability
    
    renaming: added comments
    resaving: cleaned up and added comments
    compress: cleaned up and added comments

--- jobs_alpha_3.0/rename-zeiss-files.sh
+++ jobs_alpha_3.0/rename-zeiss-files.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+# path of master file
 source /projects/pilot_spim/Christopher/pipeline_3.0/master_3.0
 
+# path of source and target files
 source_pattern=${image_file_directory}${source_pattern}
 target_pattern=${image_file_directory}${target_pattern}
 
-# --------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 i=${first_index}
 t=${first_timepoint}
@@ -14,12 +15,13 @@ t=`printf "%0${pad}d" "${t}"`
 while [ $i -le $last_index ]; do
 
         for a in "${angles_renaming[@]}"; do
-                source=${source_pattern/\{index\}/${i}}
+                
+        	source=${source_pattern/\{index\}/${i}}
                 tmp=${target_pattern/\{timepoint\}/${t}}
                 target=${tmp/\{angle\}/${a}}
 
                 echo ${source} ${target}
-
+                
                	mv ${source} ${target}
                 #cp ${source} ${target}
                 let i=i+1
